fa2a6e9d8288b9f0d34cb6b5f12d4b34cdd70a7d
Endi Shima <endy.shima@gmail.com>
2025-01-30 15:17:21 +0100

    Configured package.json and svelte.config.json for proper deployment

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,10 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      // Explicitly specify a supported Node.js runtime
+      runtime: 'nodejs20.x' // or 'nodejs18.x'/'nodejs16.x'
+    })
   },
   preprocess: vitePreprocess()
 };
